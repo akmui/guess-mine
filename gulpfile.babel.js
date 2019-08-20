63006620b72dd21d587696dc07b5436ccd10570d
LGIT <lgit@LGITui-MacBookAir.local>
2019-08-20 19:13:54 +0900

    8/20

---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+import gulp from "gulp";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+
+sass.compiler = require("node-sass");
+
+const paths = {
+    styles : {
+        src: "assets/scss/style.scss",
+        dest: "src/static/styles"
+    }
+}
+
+export function styles(){
+    return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(
+        autoprefixer({
+            browsers: ["last 2 versions"],
+            cascade: false
+        })
+    )
+    .pipe(
+        minifyCSS())
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function watchFiles(){
+    gulp.watch();
+}
